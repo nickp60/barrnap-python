13e83e51fefa039ea98927340c286876a7889893
nickp60 <nickp60@gmail.com>
2017-10-05 09:14:47 +0100

    added package data

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.7",
+    version="0.8",
 
     description='python package for barrnap ',
     # long_description=long_description,
@@ -98,9 +98,10 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    # package_data={
-    #    '': [path.join(__name__, "riboSeed", "integration_data/*")],
-    # },
+    package_data={
+       '': [path.join(__name__, "examples/*"),
+            path.join(__name__, "db/*")],
+    },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
