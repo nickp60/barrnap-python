03b40670a68b8582ff84c3644f2eb5dd1af68bab
nickp60 <nickp60@gmail.com>
2017-10-05 13:26:37 +0100

    bumped to 0.8c

--- barrnap.py
+++ barrnap.py
@@ -16,7 +16,7 @@ import pkg_resources
 # global variables
 OPSYS = platform.system().lower()
 EXE = __file__
-VERSION = "0.8b"
+VERSION = "0.8c"
 DESC = "rapid ribosomal RNA prediction"
 AUTHOR = 'Torsten Seemann <torsten.seemann@gmail.com>'
 URL = 'https://github.com/Victorian-Bioinformatics-Consortium/barrnap'
